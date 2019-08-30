8a7fe19e714754d178a13e28ddbaf3880f7014a0
leesom <870734985@qq.com>
2019-08-30 10:04:23 +0900

    创建DAO层mapper接口

---
+++ src/main/java/com/lx/seckill/mapper/SeckillMapper.java
@@ -0,0 +1,46 @@
+package com.lx.seckill.mapper;
+
+import com.lx.seckill.entity.Seckill;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @author ：leesom
+ * @date ：Created in 2019/8/30 09:27
+ * @description：商品信息相关操作接口
+ * @modified By：
+ * @version: $
+ */
+public interface SeckillMapper {
+
+    /***
+     * 查询所有商品
+     * @return
+     */
+    List<Seckill> queryAll();
+
+    /***
+     * 根据ID查询秒杀商品信息
+     * @param seckillId
+     * @return
+     */
+    Seckill queryById(long seckillId);
+
+    /***
+     * 根据偏移量查询商品信息
+     * @param offset
+     * @param limit
+     * @return
+     */
+    List<Seckill> queryByOffset(@Param("offset") int offset, @Param("limit") int limit);
+
+    /***
+     * 减少库存
+     * @param seckillId
+     * @param killTime
+     * @return
+     */
+    int reduceStock(@Param("seckillID") long seckillId, @Param("killTime")Date killTime);
+}
---
+++ src/main/java/com/lx/seckill/mapper/SeckillOrderMapper.java
@@ -0,0 +1,34 @@
+package com.lx.seckill.mapper;
+
+import com.lx.seckill.entity.SeckillOrder;
+import org.apache.ibatis.annotations.Param;
+
+import java.math.BigDecimal;
+
+/**
+ * @author ：leesom
+ * @date ：Created in 2019/8/30 09:27
+ * @description：订单信息相关操作接口
+ * @modified By：
+ * @version: $
+ */
+public interface SeckillOrderMapper {
+
+    /***
+     * 插入订单明细
+     * @param seckillId
+     * @param userPhone
+     * @param money
+     * @return 返回执行SQL更新的记录行数，如果>=1则更新成功
+     */
+    int insertOrder(@Param("seckillId") long seckillId, @Param("userPhone") long userPhone,
+                    @Param("money")BigDecimal money);
+
+    /***
+     * 查询订单明细，并且携带商品明细
+     * @param seckillId
+     * @param userPhone
+     * @return
+     */
+    SeckillOrder queryByIdWithSeckill(@Param("seckillId") long seckillId, @Param("userPhone") long userPhone);
+}
